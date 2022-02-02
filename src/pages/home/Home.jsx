7d338a7096475c3a72e7473a26ab2aa5f66c6dbf
Volodymyr <volod3419@gmail.com>
2022-02-02 17:49:38 +0200

    Added small changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,9 +5,12 @@ const Home = () => {
   return (
     <div className="Home-container">
       <DefaultPageWrapper>
-        <>
-          <p>Main content</p>
-        </>
+        <div className="Main-container">
+          <h4 className="Main-header">Main content</h4>
+          <div>
+            <p>This is updated content</p>
+          </div>
+        </div>
       </DefaultPageWrapper>
     </div>
   );
